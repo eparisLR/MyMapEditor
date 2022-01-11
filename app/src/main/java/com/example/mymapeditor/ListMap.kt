f6267b7bd5f9a5063fad48dc5b07277be3028025
Emilien <emilien.paris@etudiant.univ-lr.fr>
2022-01-11 16:48:58 +0100

    :sparkles: : starting to add create map

--- app/src/main/java/com/example/mymapeditor/ListMap.kt
+++ app/src/main/java/com/example/mymapeditor/ListMap.kt
@@ -1,11 +1,12 @@
 package com.example.mymapeditor
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
 import android.widget.Toast
-import com.example.mymapeditor.databinding.ActivityListMapBinding
+import com.example.mymapeditor.databinding.ListMapActivityBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,17 +15,24 @@ import service.MapApiClient
 import java.lang.Exception
 
 class ListMap : AppCompatActivity() {
-    private lateinit var binding: ActivityListMapBinding
+    private lateinit var binding: ListMapActivityBinding
     private var maps: List<String>? = emptyList()
-    private var oneMap: MutableList<Point>? = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityListMapBinding.inflate(layoutInflater)
+        binding = ListMapActivityBinding.inflate(layoutInflater)
 
         setContentView(binding.root);
 
         getMaps()
+
+        binding.createMap.setOnClickListener {
+            val intent = Intent( this, ModifyMap::class.java)
+            // Cette ligne sert à injecter des paramètres UTILE POUR LA MODIF D'UNE CARTE
+            //intent.putExtra(SecondaryActivity.EXTRA_KEY , binding.saisieNom.text.toString())
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun getMaps() {
@@ -37,31 +45,6 @@ class ListMap : AppCompatActivity() {
                     println(maps)
                     val listView = findViewById<ListView>(R.id.listview)
                     listView.adapter = maps?.let { MapAdapter(this@ListMap, it) }
-                } else {
-                    Toast.makeText(
-                        this@ListMap,
-                        "Error occurred : ${response.message()}",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            } catch (e: Exception) {
-                Toast.makeText(
-                    this@ListMap,
-                    "Error occurred : ${e.message}",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
-    }
-
-    private fun getOneMap(){
-        GlobalScope.launch(Dispatchers.Main) {
-            try {
-                val response = MapApiClient.mapApiService.getMapById(1)
-                if (response.isSuccessful && response.body() != null) {
-                    val content = response.body()
-                    oneMap = content
-                    println(maps)
                 } else {
                     Toast.makeText(
                         this@ListMap,
--- app/src/main/java/com/example/mymapeditor/ModifyMap.kt
+++ app/src/main/java/com/example/mymapeditor/ModifyMap.kt
@@ -1,11 +1,77 @@
 package com.example.mymapeditor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mymapeditor.databinding.ModifyMapActivityBinding
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import model.Point
+import service.MapApiClient
+import java.lang.Exception
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.Mapbox
 
 class ModifyMap : AppCompatActivity() {
+    private lateinit var binding: ModifyMapActivityBinding
+    private var oneMap: MutableList<Point>? = arrayListOf()
+    private lateinit var mapview: MapView
+
+    companion object {
+        val EXTRA_KEY = "MAP_NAME"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify_map)
+        Mapbox.getInstance(this, getString(R.string.access_token))
+        binding = ModifyMapActivityBinding.inflate(layoutInflater)
+
+        setContentView(binding.root);
+
+        // Displaying the map
+
+        mapview = findViewById(R.id.mapview);
+        mapview.onCreate(savedInstanceState)
+        // Obtention de la carte de manière asynchrone
+        mapview.getMapAsync { map ->
+            // On choisi un style (et on ne fait rien avec)
+            map.setStyle(Style.OUTDOORS) { style ->
+            }
+        }
+
+        // Displaying the spinner
+
+        binding.returnButton.setOnClickListener {
+            val intent = Intent(this, ListMap::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun getOneMap(){
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val response = MapApiClient.mapApiService.getMapById(1)
+                if (response.isSuccessful && response.body() != null) {
+                    val content = response.body()
+                    oneMap = content
+                } else {
+                    Toast.makeText(
+                        this@ModifyMap,
+                        "Error occurred : ${response.message()}",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            } catch (e: Exception) {
+                Toast.makeText(
+                    this@ModifyMap,
+                    "Error occurred : ${e.message}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
